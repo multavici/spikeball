cfd28d99e39278c01f5b5bf76cb84c90ac2742d2
Falk Van der Meirsch <falk@codedor.be>
2018-12-28 18:02:20 +0100

    change mail sender

--- config.py
+++ config.py
@@ -15,6 +15,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['multavici@gmail.com']
+    ADMINS = ['noreply@spikeballgent.be']
 
     POSTS_PER_PAGE = 25
