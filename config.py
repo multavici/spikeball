3724c90c7af9a1f7ee740ec30b884f0a00890113
flaport <floris.laporte@gmail.com>
2019-02-18 22:11:10 +0100

    added flaskenv example

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 import os
+
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, ".flaskenv"))
+
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URI"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['noreply@spikeballgent.be']
+
+    MAIL_SERVER = os.environ["MAIL_SERVER"]
+    MAIL_PORT = int(os.environ["MAIL_PORT"])
+    MAIL_USE_TLS = os.environ["MAIL_USE_TLS"]
+    MAIL_USERNAME = os.environ["MAIL_USERNAME"]
+    MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
+    ADMINS = ["noreply@spikeballgent.be"]
 
     POSTS_PER_PAGE = 25
